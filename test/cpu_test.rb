334499469847ffbdd093e5ecad85e7de825d09e8
Jano González <info@janogonzalez.com>
2014-04-18 19:37:31 +0200

    Refactors CPU test

--- test/cpu_test.rb
+++ test/cpu_test.rb
@@ -17,19 +17,17 @@ describe ElOcho::CPU do
 
   describe "with a 7XNN instruction" do
     it "adds the NN value into register X" do
-      @cpu.load [0x72, 0x80, 0x72, 0x02, 0x72]
-      @cpu.step
-      @cpu.v[2].must_equal 0x80
-      @cpu.pc.must_equal 0x202
-      @cpu.step
+      @cpu.load [0x72, 0x80,
+                 0x72, 0x02]
+      2.times { @cpu.step }
       @cpu.v[2].must_equal 0x82
       @cpu.pc.must_equal 0x204
     end
 
     it "applies the 0xFF mask to the result" do
-      @cpu.load [0x72, 0xFF, 0x72, 0x82]
-      @cpu.step
-      @cpu.step
+      @cpu.load [0x72, 0xFF,
+                 0x72, 0x82]
+      2.times { @cpu.step }
       @cpu.v[2].must_equal 0x81
       @cpu.pc.must_equal 0x204
     end
@@ -37,9 +35,9 @@ describe ElOcho::CPU do
 
   describe "with a 8XY0 instruction" do
     it "sets register X to the value of register Y" do
-      @cpu.load [0x62, 0x82, 0x81, 0x20]
-      @cpu.step
-      @cpu.step
+      @cpu.load [0x62, 0x82,
+                 0x81, 0x20]
+      2.times { @cpu.step }
       @cpu.v[1].must_equal 0x82
       @cpu.pc.must_equal 0x204
     end
@@ -47,7 +45,9 @@ describe ElOcho::CPU do
 
   describe "with a 8XY1 instruction" do
     it "sets register X to the OR between registers X and Y" do
-      @cpu.load [0x62, 0xCC, 0x61, 0x11, 0x81, 0x21]
+      @cpu.load [0x62, 0xCC,
+                 0x61, 0x11,
+                 0x81, 0x21]
       3.times { @cpu.step }
       @cpu.v[1].must_equal 0xDD
       @cpu.pc.must_equal 0x206
@@ -56,7 +56,9 @@ describe ElOcho::CPU do
 
   describe "with a 8XY2 instruction" do
     it "sets register X to the AND between registers X and Y" do
-      @cpu.load [0x62, 0xCC, 0x61, 0xAA, 0x81, 0x22]
+      @cpu.load [0x62, 0xCC,
+                 0x61, 0xAA,
+                 0x81, 0x22]
       3.times { @cpu.step }
       @cpu.v[1].must_equal 0x88
       @cpu.pc.must_equal 0x206
@@ -65,7 +67,9 @@ describe ElOcho::CPU do
 
   describe "with a 8XY3 instruction" do
     it "sets register X to the XOR between registers X and Y" do
-      @cpu.load [0x62, 0xCC, 0x61, 0xAA, 0x81, 0x23]
+      @cpu.load [0x62, 0xCC,
+                 0x61, 0xAA,
+                 0x81, 0x23]
       3.times { @cpu.step }
       @cpu.v[1].must_equal 0x66
       @cpu.pc.must_equal 0x206
